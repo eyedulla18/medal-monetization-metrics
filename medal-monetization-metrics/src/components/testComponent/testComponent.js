3f6496f199863c94c4b378a089eb88e238bac6f6
Eashan Yedulla <eyedulla@Eashans-MacBook-Air.local>
2024-08-11 15:20:51 -0400

    add react component update deployment settings

---
+++ medal-monetization-metrics/src/components/testComponent/testComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const TestComponent = () => (
+  <div>
+    testComponent Component
+  </div>
+);
+
+TestComponent.propTypes = {};
+
+TestComponent.defaultProps = {};
+
+export default TestComponent;
