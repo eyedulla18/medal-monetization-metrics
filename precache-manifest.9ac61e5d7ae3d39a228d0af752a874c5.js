79bd065ae08073b5eea1f511b2dfef390fd52313
Eashan Yedulla <eyedulla@Eashans-MacBook-Air.local>
2024-08-11 15:23:25 -0400

    Updates

---
+++ precache-manifest.9ac61e5d7ae3d39a228d0af752a874c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13364fa658f74b49325927675cf7a6ef",
+    "url": "/medal-monetization-metrics/index.html"
+  },
+  {
+    "revision": "7e731730485bf912e290",
+    "url": "/medal-monetization-metrics/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/medal-monetization-metrics/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/medal-monetization-metrics/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "7e731730485bf912e290",
+    "url": "/medal-monetization-metrics/static/js/main.b46ff434.chunk.js"
+  },
+  {
+    "revision": "0ff352ba042fc393a84d",
+    "url": "/medal-monetization-metrics/static/js/runtime~main.7529ad1f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/medal-monetization-metrics/static/media/logo.06e73328.svg"
+  }
+]);
